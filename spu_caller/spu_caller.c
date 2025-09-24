b97bc04085406f0b72a6be0bd35d335f2ce386c2
Model Citizen <model.citizen.ps3@gmail.com>
2025-09-24 01:40:46 -0400

    added spu_caller, spu_mask, spu_shright, spu_subtract

---
+++ spu_caller/spu_caller.c
@@ -0,0 +1,13 @@
+// calling an assembly coded function
+
+#include <stdio.h>
+
+extern int func(int, int);
+
+int main(int argc, char **argv)
+{
+    int res = func(5, 2);
+    printf("Result = %d\n", res);
+    return 0;
+}
+
